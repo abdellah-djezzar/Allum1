523986414328a00b0616bd95821de7a076e4e0b0
abdellah-djezzar <abdellah.djezzar@epitech.eu>
2021-04-18 20:16:19 +0200

    rendu final

--- allum1.php
+++ allum1.php
@@ -18,31 +18,31 @@ echo str_pad("", $calcu, " ", STR_PAD_LEFT);
 function Mother($x){
     $i = 0;
     usleep(1000000);
-    echo PHP_EOL."AI 's turn ...".PHP_EOL;
+    echo PHP_EOL."Tour de l'IA ...".PHP_EOL;
     usleep(1000000);
     if($x ==1)
     {
-        echo "I lost ... snif ... but I'll get you next time !!";
+        echo "Oh NON,j'ai perdu!!";
     }
     if(($x - 1)% 4 == 1)
     {
         $nbr_ordi = 1;
         $nbr_allum = $x -$nbr_ordi;
-        echo "AI removed ". $nbr_ordi ." match ( es )";
+        echo "L'IA enlève ". $nbr_ordi ." allumettes";
         Grille($nbr_allum, $nbr_ordi);
     } else if (($x - 2) % 4 == 1) {
         $nbr_ordi = 2;
         $nbr_allum = $x -$nbr_ordi;
-        echo "AI removed ". $nbr_ordi ." match ( es )";
+        echo "L'IA enlève ". $nbr_ordi ." allumettes";
         Grille($nbr_allum, $nbr_ordi);
     } else if (($x - 3) % 4 == 1) {
         $nbr_ordi = 3;
         $nbr_allum = $x -$nbr_ordi;
-        echo "AI removed ". $nbr_ordi ." match ( es )";
+        echo "L'IA enlève ". $nbr_ordi ." allumettes";
         Grille($nbr_allum, $nbr_ordi);
     } else {
         $nbr_allum = $x - (1+rand(1, 3));
-        echo "AI removed match ( es )";
+        echo "L'IA enlève ". $nbr_ordi ." allumettes";
         Grille($nbr_allum, $nbr_ordi);
     }
 }
@@ -50,9 +50,9 @@ function Mother($x){
 // Suppression de maximum 3 allumettes
 function delAllum($limit){
     $i=0;
-    echo PHP_EOL."Your turn : Matches : ";
+    echo PHP_EOL."Votre Tour !Combien d'allumettes voulez-vous enlever? ";
     $delAllum = readline();
-    echo "Player removed ".$delAllum." match ( es )" . PHP_EOL;
+    echo "Vous avez enlever ".$delAllum." allumettes" . PHP_EOL;
     // echo $delAllum;
 
     if($delAllum >= 1 && $delAllum <= 3 && $delAllum != 0)
@@ -67,20 +67,20 @@ function delAllum($limit){
         }
 
     } else if ($delAllum < 0 || !is_string($delAllum)){
-        echo "Error : invalid input ( positive number expected )" . PHP_EOL;
+        echo "Error : invalid input ( Un nombre valide doit être écris! )" . PHP_EOL;
         delAllum($limit);
     } else if ($delAllum == 0){
-        echo "Error : you have to remove at least one match";
+        echo "Error : Vous devez enlever au moins 1 allumettes!";
         delAllum($limit);
     } else if ($delAllum >= 4){
-        echo "Error : not enough matches Matches :" . PHP_EOL;
-        echo "Error : invalid input ( positive number expected )";
+        echo "Error : La valeur est trop grande!" . PHP_EOL;
+        echo "Error : invalid input ( Un nombre valide doit être écris! )";
         delAllum($limit);
     }
     if(isset($result)){
         // echo PHP_EOL."Player removed ".$delAllum." match ( es )" . PHP_EOL;
         if($result == 0){
-            echo PHP_EOL."You lost , too bad ...".PHP_EOL;
+            echo PHP_EOL."Vous avez perdu!".PHP_EOL;
             exit;
         }
         Mother($result);
